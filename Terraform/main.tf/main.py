ec72a8e6978fe0023c63acd7a51b2b05bdf92c9f
Zinhle Sibisi <107902020+zsibisi@users.noreply.github.com>
2022-08-17 20:39:51 -0700

    Add files via upload

---
+++ Terraform/main.tf/main.py
@@ -0,0 +1,12 @@
+
+
+#identifier = value
+#variable rules
+#1. alphanumeric characters and underscores
+#A-Z 0-9 _
+#must start with a letter or _ character
+#order matters
+price1 = 10
+price2 =20
+total = price1 + price2
+print(total)
